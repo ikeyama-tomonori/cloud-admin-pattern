22875c69a8b802889bfc36e2360da9a6b255b5be
Ikeyama Tomonori <ikeyama.tomonori@gmail.com>
2022-07-15 12:23:29 +0900

    C#のテストを追加

---
+++ server/test/Server.Main.WebApi.Test/Middleware/EfRestExtentionTest.cs
@@ -0,0 +1,32 @@
+﻿namespace Server.Main.WebApi.Test.Middleware;
+
+using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using Xunit.Abstractions;
+
+public class EfRestExtentionTest
+{
+    private readonly ITestOutputHelper output;
+
+    public EfRestExtentionTest(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
+    [Fact]
+    public async Task EfRest経由でアクセスできる()
+    {
+        var factory = new TestWebApplicationFactory(this.output);
+        var client = factory.CreateClient();
+        var guid = Guid.NewGuid();
+        var name = $"EfRestTest {guid}";
+        var response = await client.PostAsJsonAsync("/api/organizations", new { Name = name });
+        var created = await response.Content.ReadFromJsonAsync<Model.Organization>();
+
+        Assert.Equal(name, created?.Name);
+    }
+}
---
+++ server/test/Server.Main.WebApi.Test/Service/AddAppDbContextExtentionTest.cs
@@ -0,0 +1,30 @@
+﻿namespace Server.Main.WebApi.Test.Service;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Server.Main.WebApi.Service;
+using Server.Repository.AppDb;
+using Xunit;
+
+public class AddAppDbContextExtentionTest
+{
+    [Fact]
+    public void CloudCqsOptionsを追加できる()
+    {
+        var builder = WebApplication.CreateBuilder();
+        builder.Services.AddAppDbContext();
+
+        var serviceDescriptors = builder.Services.Where(
+            serviceDescriptor => serviceDescriptor.ServiceType == typeof(AppDbContext)
+        );
+
+        Assert.Single(serviceDescriptors);
+
+        var app = builder.Build();
+
+        using var scope = app.Services.CreateScope();
+        var db = scope.ServiceProvider.GetService<AppDbContext>();
+
+        Assert.NotNull(db);
+    }
+}
